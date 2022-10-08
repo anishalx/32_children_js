6dc6fe7df2a28ea913b422243678fb9e040a111c
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-08 14:58:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+// console.log()
+//for convert into arrays
+let ar = Array.from(document.body.childNodes)
+console.log(ar)
